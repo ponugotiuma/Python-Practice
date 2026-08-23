5f71557908302fb425cbe00dfa1a51444250ecc5
Uma Ponugoti <umaponugoti18@gmail.com>
2026-08-23 17:58:03 +0530

    week 2

---
+++ week 2/code 3.py
@@ -0,0 +1,13 @@
+# Printing numbers
+for number in range(1, 6):
+    print(number)
+
+print("Loop completed")
+
+#list & loop
+skills = ["Python", "SQL", "Power BI"]
+
+for skill in skills:
+    print("Uma is learning", skill)
+
+print("Learning continues!")
