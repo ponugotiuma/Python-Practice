eb459b92b940de23fcaf9b4b7a2a422ecd6c22fe
Uma Ponugoti <umaponugoti18@gmail.com>
2026-08-31 18:32:17 +0530

    ques1.py

---
+++ Python_Basics_Test/ques1.py
@@ -0,0 +1,9 @@
+#Write a Python program to print the company name, product name, and service name on separate lines.
+
+company="Google"
+product="Gmail"
+service="For professional Contacting"
+
+print("Company Name:", company)
+print("Product Name:", product)
+print("Service Name:", service)
