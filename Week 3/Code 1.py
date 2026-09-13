8e7718710589d62f3cff800d102921c0f14bae20
Uma Ponugoti <umaponugoti18@gmail.com>
2026-09-13 20:08:18 +0530

    Intro to Sets

---
+++ Week 3/Code 1.py
@@ -0,0 +1,19 @@
+#Creating Lists
+a=[1,2,3]
+##empty list
+b=[]
+##iteration with keyword list
+c=list("abc")
+##ranging the list
+d=list(range(5))
+##repitition of elements
+e=[0]*5
+##comprehension from for loop
+f=[x*x for x in range(5)]
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
