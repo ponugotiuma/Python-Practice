e94e80cee5a15cd9930a076d86216a8d67819ac9
Uma Ponugoti <umaponugoti18@gmail.com>
2026-09-17 18:49:12 +0530

    slicing

---
+++ Week 3/code 5.py
@@ -0,0 +1,12 @@
+#SLICING in lists
+s=[0,1,2,3,4,5,6,7,8,9]
+print(s[2:5]) 
+print(s[:3])
+print(s[7:])
+print(s[:]) #full copy of the list
+print(s[::2]) #every 2nd element in the list
+print(s[1::2]) #every odd index element
+print(s[::-1]) #reverses the list
+print(s[-3:]) #last 3 elements
+print(s[:-3]) #all except last three
+print(s[5:2:-1]) #backwards
